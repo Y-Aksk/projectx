9418afa89fff4c659ce2ec441c769e5696a2e694
Y-Aksk <akasaka@solekia.co.jp>
2022-06-01 19:04:10 +0900

    初回コミット

---
+++ src/main/java/com/solekia/testproj/ProjectxApplication.java
@@ -0,0 +1,13 @@
+package com.solekia.testproj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectxApplication.class, args);
+	}
+
+}
